63e5c5d630d5b66f34cd1c3c7010ce32f84fd741
Ayman Mohamed <aymantaha632@gmail.com>
2022-06-27 21:41:07 -0400

    implementing messages
    -error and success messages in the view

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -4,6 +4,8 @@ import fracty from 'fracty';
 class RecipeView {
   #parentEl = document.querySelector('.recipe');
   #data;
+  #errorMsg = "We couldn't find that recipe. Please find another one";
+  #successMsg = 'lolloloo';
 
   render(data) {
     this.#data = data;
@@ -16,7 +18,7 @@ class RecipeView {
     this.#parentEl.innerHTML = '';
   }
 
-  loadSpinner = function () {
+  loadSpinner() {
     const markup = `
       <div class="spinner">
         <svg>
@@ -24,9 +26,39 @@ class RecipeView {
         </svg>
       </div>
     `;
-    this.#parentEl.innerHTML = '';
+    this.#clear();
     this.#parentEl.insertAdjacentHTML('afterbegin', markup);
-  };
+  }
+
+  renderError(msg = this.#errorMsg) {
+    const markup = `
+        <div class="error">
+            <div>
+              <svg>
+                <use href="${icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${msg}</p>
+        </div> 
+    `;
+    this.#clear();
+    this.#parentEl.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderSuccess(msg = this.#successMsg) {
+    const markup = `
+        <div class="message">
+            <div>
+              <svg>
+                <use href="${icons}#icon-smile"></use>
+              </svg>
+            </div>
+            <p>${msg}</p>
+        </div> 
+    `;
+    this.#clear();
+    this.#parentEl.insertAdjacentHTML('afterbegin', markup);
+  }
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(event =>
