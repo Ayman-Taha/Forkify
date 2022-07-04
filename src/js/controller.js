91f19d5729df8e5c0d103fffd04d04596bd4c078
Ayman Mohamed <aymantaha632@gmail.com>
2022-07-04 16:09:32 -0400

    implementing bookmarks view
    - refactoring to DRY
    - bookmarks view
    - remove some unused imports

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -17,6 +18,7 @@ const controlRecipes = async function () {
     recipeView.loadSpinner();
 
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     await model.loadRecipe(id);
     const { recipe } = model.state;
@@ -54,7 +56,10 @@ const controlAddBookmark = function () {
   } else {
     model.addBookmark(model.state.recipe);
   }
+
   recipeView.update(model.state.recipe);
+
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
